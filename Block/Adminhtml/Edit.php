f79593743fe9cc23cdcf3fbacc351bacf3c78bad
Enrico <telefonosuci@gmail.com>
2018-01-11 15:25:46 +0100

    Admin new post

---
+++ Block/Adminhtml/Edit.php
@@ -0,0 +1,24 @@
+<?php
+namespace ThinkOpen\Blog\Block\Adminhtml;
+
+class Edit extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \ThinkOpen\Blog\Model\PostFactory $postFactory)
+    {
+        $this->_postFactory = $postFactory;
+        parent::__construct($context);
+    }
+
+
+    public function getPostData()
+    {
+        $params = $this->getRequest()->getParams();
+        $id = $this->getRequest()->getParam('idpost');
+        $post = $this->_postFactory->create();
+        
+		$postData = $post->load($id);
+        return $postData;
+    }
+}
