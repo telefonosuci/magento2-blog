e9cdbc53d247abab61e9730f248f7e678ccfa282
Enrico <telefonosuci@gmail.com>
2018-01-11 13:36:14 +0100

    Admin post edit

--- Block/Adminhtml/Display.php
+++ Block/Adminhtml/Display.php
@@ -3,11 +3,19 @@ namespace ThinkOpen\Blog\Block\Adminhtml;
 
 class Display extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var \Magento\Core\Helper\Data $helper
+     */
+    //protected $helper;
+
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \ThinkOpen\Blog\Model\PostFactory $postFactory)
+        \ThinkOpen\Blog\Model\PostFactory $postFactory
+        //\Magento\Core\Helper\Data $helper
+        )
     {
         $this->_postFactory = $postFactory;
+        //$this->helper = $helper;
         parent::__construct($context);
     }
     
@@ -19,4 +27,11 @@ class Display extends \Magento\Framework\View\Element\Template
         return $collection;
         
     }
+
+    public function getEditURL($postID){
+        //$url = Mage::helper("adminhtml")->getUrl("ThinkOpen_Blog/post/edit/idpost/*", $postID);
+        //$url = $this->helper->getUrl("ThinkOpen_Blog/post/edit/idpost/*", $postID);
+        return "pippo";
+    }
+
 }
