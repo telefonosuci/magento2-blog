81dbc13a8570f938153594d40d1fb1e378d77c38
Enrico <telefonosuci@gmail.com>
2018-01-12 16:52:55 +0100

    working on post form
    https://magento.stackexchange.com/questions/149429/magento-2-admin-grid-from-file-collection

---
+++ Model/ResourceModel/Post/PostDataProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ThinkOpen\Blog\Model\ResourceModel\Post;
+
+use ThinkOpen\Blog\Model\ResourceModel\Post\CollectionFactory;
+use ThinkOpen\Blog\Model\Post;
+
+class PostDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $postCollectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $postCollectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $postCollectionFactory->create();
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+
+
+    public function getData()
+    {
+
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+
+        $items = $this->collection->getItems();
+        $this->loadedData = array();
+        /** @var Post $post */
+        foreach ($items as $post) {
+            // our fieldset is called "contact" or this table so that magento can find its datas:
+            $this->loadedData[$contact->getId()]['post'] = $contact->getData();
+        }
+
+        return $this->loadedData;
+
+    }
+
+
+
+}
